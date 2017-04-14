8ad005878a1e271d7c043929036f8dc18c279a61
Sung E. Choi <sungeunchoi.kr@gmail.com>
2017-04-14 08:45:30 -0500

    typed all the tables

--- main.hs
+++ main.hs
@@ -1,18 +1,9 @@
-{-# LANGUAGE CPP, ForeignFunctionInterface #-}
-
 import Foreign
 import Foreign.C.Types
 import Foreign.C.String
 import Data.Map (Map)
 import qualified Data.Map as Map
---import Control.Conditional
---import Control.Monad
 import qualified Scanner
-
---(==:) :: ( Eq a,Monad m) => m a -> m a -> m Bool
---(==:) = liftM2 (==)
-
---main = ifM (getLine ==: getLine) (print "hit") (print "miss")
 
 data Nonterminal = N_SS | N_BLOCK | N_MORE_BLOCKS | N_CLASS | N_METHOD | N_MODIFIER |
         N_ID_LIST | N_MORE_IDS | N_METHODS | N_MORE_METHODS | N_STMTS | N_STMT | N_MORE_STMTS |
@@ -26,26 +17,60 @@ data Terminal = IDENTIFIER| CONSTANT |  CLASS | SSALC | FORALL | FORMYSELFONLY |
             deriving (Enum, Eq, Ord, Show)
 
 data Symbol = Terminal Terminal | Nonterminal Nonterminal deriving (Show)
---data Rule = Rule Nonterminal [Symbol]
-
---constructGrammarTable :: Nonterminal -> Symbol --Either Terminal Nonterminal
---constructGrammarTable n
---    | n == SS                       = N BLOCK
---    | n == BLOCK                    = T EPSILON
---    | n == MORE_BLOCKS              = T EPSILON
-
---gammarTable :: [Rule]
---gammarTable = [(Rule SS $ N BLOCK)]
 
 indexedCFGRules :: Map Int (Nonterminal,[Symbol])
 indexedCFGRules = Map.fromList(
     [
-         (1, (N_SS           , [(Nonterminal N_BLOCK), (Nonterminal N_MORE_BLOCKS)]))
-        ,(2, (N_MORE_BLOCKS  , [Terminal EPSILON]))
-        ,(3, (N_MORE_BLOCKS  , [(Nonterminal N_BLOCK), (Nonterminal N_MORE_BLOCKS)]))
-        ,(4, (N_BLOCK        , [(Nonterminal N_CLASS)]))
-        ,(5, (N_BLOCK        , [(Nonterminal N_METHOD)]))
-        ,(6, (N_CLASS        , [(Terminal CLASS), (Terminal IDENTIFIER), (Nonterminal N_MODIFIER), (Terminal LPAREN), (Nonterminal N_ID_LIST), (Terminal RPAREN), (Terminal SSALC)]))
+         ( 1, (N_SS           , [Nonterminal N_BLOCK, Nonterminal N_MORE_BLOCKS]))
+        ,( 2, (N_MORE_BLOCKS  , [Terminal EPSILON]))
+        ,( 3, (N_MORE_BLOCKS  , [Nonterminal N_BLOCK, Nonterminal N_MORE_BLOCKS]))
+        ,( 4, (N_BLOCK        , [Nonterminal N_CLASS]))
+        ,( 5, (N_BLOCK        , [Nonterminal N_METHOD]))
+        ,( 6, (N_CLASS        , [(Terminal CLASS), (Terminal IDENTIFIER), (Nonterminal N_MODIFIER), (Terminal LPAREN), (Nonterminal N_ID_LIST), (Terminal RPAREN), (Terminal SSALC)]))
+        ,( 7, (N_MODIFIER     , [Terminal FORALL]))
+        ,( 8, (N_MODIFIER     , [Terminal FORMYSELFONLY]))
+        ,( 9, (N_ID_LIST      , [Terminal IDENTIFIER, Nonterminal N_MORE_IDS]))
+        ,(10, (N_MORE_IDS     , [Terminal EPSILON]))
+        ,(11, (N_MORE_IDS     , [Terminal IDENTIFIER, Nonterminal N_MORE_IDS]))
+        ,(12, (N_METHODS      , [Nonterminal N_METHOD, Nonterminal N_MORE_METHODS]))
+        ,(13, (N_MORE_METHODS , [Terminal EPSILON]))
+        ,(14, (N_MORE_METHODS , [Nonterminal N_METHOD, Nonterminal N_MORE_METHODS]))
+        ,(15, (N_METHOD       , [Terminal METHOD, Terminal IDENTIFIER, Nonterminal N_STMTS, Terminal DOHTEM]))
+        ,(16, (N_STMTS        , [Nonterminal N_STMT, Terminal SEMICOLON, Nonterminal N_MORE_STMTS]))
+        ,(17, (N_MORE_STMTS   , [Terminal EPSILON]))
+        ,(18, (N_MORE_STMTS   , [Nonterminal N_STMT, Terminal SEMICOLON, Nonterminal N_MORE_STMTS]))
+        ,(19, (N_STMT         , [Nonterminal N_DECL]))
+        ,(20, (N_STMT         , [Nonterminal N_INPUT]))
+        ,(21, (N_STMT         , [Nonterminal N_OUTPUT]))
+        ,(22, (N_STMT         , [Nonterminal N_COND]))
+        ,(23, (N_STMT         , [Nonterminal N_LOOP]))
+        ,(24, (N_STMT         , [Nonterminal N_ASMT]))
+        ,(25, (N_STMT         , [Nonterminal N_EXIT]))
+        ,(26, (N_DECL         , [Nonterminal N_TYPE, Nonterminal N_ID_LIST]))
+        ,(27, (N_TYPE         , [Terminal NUMBER]))
+        ,(28, (N_TYPE         , [Terminal BOOLEAN]))
+        ,(29, (N_INPUT        , [Terminal SEE, Nonterminal N_ID_LIST]))
+        ,(30, (N_OUTPUT       , [Terminal SHOW, Nonterminal N_ID_LIST]))
+        ,(31, (N_COND         , [Terminal IF, Nonterminal N_BOOL_EXPR, Nonterminal N_STMTS, Terminal OTHERWISE, Nonterminal N_STMTS, Terminal ONLYIF, Nonterminal N_BOOL_EXPR]))
+        ,(32, (N_LOOP         , [Terminal LOOP, Nonterminal N_STMTS, Terminal POOL]))
+        ,(33, (N_ASMT         , [Terminal COPY, Nonterminal N_EXPR, Terminal INTO, Nonterminal N_ID_LIST]))
+        ,(34, (N_EXIT         , [Terminal EXIT, Terminal IFNOT, Nonterminal N_BOOL_EXPR]))
+        ,(35, (N_EXPR         , [Nonterminal N_BOOL_EXPR]))
+        ,(36, (N_EXPR         , [Nonterminal N_ARITH_EXPR]))
+        ,(37, (N_BOOL_EXPR    , [Terminal NONO, Nonterminal N_BOOL_EXPR]))
+        ,(38, (N_BOOL_EXPR    , [Terminal NOTFALSE, Nonterminal N_BOOL_TAIL]))
+        ,(39, (N_BOOL_EXPR    , [Terminal NOTTRUE, Nonterminal N_BOOL_TAIL]))
+        ,(40, (N_BOOL_EXPR    , [Nonterminal N_TEST, Nonterminal N_ARITH_EXPR, Nonterminal N_ARITH_EXPR]))
+        ,(41, (N_BOOL_TAIL    , [Terminal EPSILON]))
+        ,(42, (N_BOOL_TAIL    , [Terminal AND, Nonterminal N_BOOL_EXPR]))
+        ,(43, (N_BOOL_TAIL    , [Terminal OR, Nonterminal N_BOOL_EXPR]))
+        ,(44, (N_TEST         , [Terminal LESS]))
+        ,(45, (N_TEST         , [Terminal NOTEQUAL]))
+        ,(46, (N_ARITH_EXPR   , [Terminal ADD, Nonterminal N_ARITH_EXPR, Nonterminal N_ARITH_EXPR]))
+        ,(47, (N_ARITH_EXPR   , [Terminal STAR, Nonterminal N_ARITH_EXPR, Nonterminal N_ARITH_EXPR]))
+        ,(48, (N_ARITH_EXPR   , [Terminal LPAREN, Terminal IDENTIFIER, Terminal RPAREN]))
+        ,(49, (N_ARITH_EXPR   , [Terminal IDENTIFIER]))
+        ,(50, (N_ARITH_EXPR   , [Terminal CONSTANT]))
     ])
 
 parseTable :: Map (Nonterminal, Terminal) Int
@@ -71,6 +96,97 @@ parseTable = Map.fromList(
         ,((N_MORE_IDS      , RPAREN       )      , 10)
         ,((N_MORE_IDS      , SEMICOLON    )      , 10)
         ,((N_MORE_IDS      , IDENTIFIER   )      , 11)
+
+        ,((N_METHODS       , METHOD       )      , 12)
+
+        ,((N_MORE_METHODS  , SSALC        )      , 13)
+        ,((N_MORE_METHODS  , METHOD       )      , 14)
+
+        ,((N_METHOD        , METHOD       )      , 15)
+
+        ,((N_STMTS         , NUMBER       )      , 16)
+        ,((N_STMTS         , BOOLEAN      )      , 16)
+        ,((N_STMTS         , SEE          )      , 16)
+        ,((N_STMTS         , SHOW         )      , 16)
+        ,((N_STMTS         , IF           )      , 16)
+        ,((N_STMTS         , LOOP         )      , 16)
+        ,((N_STMTS         , COPY         )      , 16)
+        ,((N_STMTS         , EXIT         )      , 16)
+
+        ,((N_MORE_STMTS    , DOHTEM       )      , 17)
+        ,((N_MORE_STMTS    , OTHERWISE    )      , 17)
+        ,((N_MORE_STMTS    , ONLYIF       )      , 17)
+        ,((N_MORE_STMTS    , POOL         )      , 17)
+
+        ,((N_MORE_STMTS    , NUMBER       )      , 18)
+        ,((N_MORE_STMTS    , BOOLEAN      )      , 18)
+        ,((N_MORE_STMTS    , SEE          )      , 18)
+        ,((N_MORE_STMTS    , SHOW         )      , 18)
+        ,((N_MORE_STMTS    , IF           )      , 18)
+        ,((N_MORE_STMTS    , LOOP         )      , 18)
+        ,((N_MORE_STMTS    , COPY         )      , 18)
+        ,((N_MORE_STMTS    , EXIT         )      , 18)
+
+        ,((N_STMT          , NUMBER       )      , 19)
+        ,((N_STMT          , BOOLEAN      )      , 19)
+        ,((N_STMT          , SEE          )      , 20)
+        ,((N_STMT          , SHOW         )      , 21)
+        ,((N_STMT          , IF           )      , 22)
+        ,((N_STMT          , LOOP         )      , 23)
+        ,((N_STMT          , COPY         )      , 24)
+        ,((N_STMT          , EXIT         )      , 25)
+
+        ,((N_DECL          , NUMBER       )      , 26)
+        ,((N_DECL          , BOOLEAN      )      , 26)
+
+        ,((N_TYPE          , NUMBER       )      , 27)
+        ,((N_TYPE          , BOOLEAN      )      , 28)
+
+        ,((N_INPUT         , SEE          )      , 29)
+        ,((N_OUTPUT        , SHOW         )      , 30)
+        ,((N_COND          , IF           )      , 31)
+        ,((N_LOOP          , LOOP         )      , 32)
+        ,((N_ASMT          , COPY         )      , 33)
+        ,((N_EXIT          , EXIT         )      , 34)
+
+        ,((N_EXPR          , NONO         )      , 35)
+        ,((N_EXPR          , NOTFALSE     )      , 35)
+        ,((N_EXPR          , NOTTRUE      )      , 35)
+        ,((N_EXPR          , LESS         )      , 35)
+        ,((N_EXPR          , NOTEQUAL     )      , 35)
+        ,((N_EXPR          , ADD          )      , 36)
+        ,((N_EXPR          , STAR         )      , 36)
+        ,((N_EXPR          , LPAREN       )      , 36)
+        ,((N_EXPR          , IDENTIFIER   )      , 36)
+        ,((N_EXPR          , CONSTANT     )      , 36)
+
+        ,((N_BOOL_EXPR     , NONO         )      , 37)
+        ,((N_BOOL_EXPR     , NOTFALSE     )      , 38)
+        ,((N_BOOL_EXPR     , NOTTRUE      )      , 39)
+        ,((N_BOOL_EXPR     , LESS         )      , 40)
+        ,((N_BOOL_EXPR     , NOTEQUAL     )      , 40)
+
+        ,((N_BOOL_TAIL     , NUMBER       )      , 41)
+        ,((N_BOOL_TAIL     , BOOLEAN      )      , 41)
+        ,((N_BOOL_TAIL     , SEE          )      , 41)
+        ,((N_BOOL_TAIL     , SHOW         )      , 41)
+        ,((N_BOOL_TAIL     , IF           )      , 41)
+        ,((N_BOOL_TAIL     , LOOP         )      , 41)
+        ,((N_BOOL_TAIL     , COPY         )      , 41)
+        ,((N_BOOL_TAIL     , EXIT         )      , 41)
+        ,((N_BOOL_TAIL     , INTO         )      , 41)
+
+        ,((N_BOOL_TAIL     , AND          )      , 42)
+        ,((N_BOOL_TAIL     , OR           )      , 43)
+
+        ,((N_TEST          , LESS         )      , 44)
+        ,((N_TEST          , NOTEQUAL     )      , 45)
+
+        ,((N_ARITH_EXPR    , ADD          )      , 46)
+        ,((N_ARITH_EXPR    , STAR         )      , 47)
+        ,((N_ARITH_EXPR    , LPAREN       )      , 48)
+        ,((N_ARITH_EXPR    , IDENTIFIER   )      , 49)
+        ,((N_ARITH_EXPR    , CONSTANT     )      , 50)
     ])
 
 cc_load_source :: String -> IO ()
